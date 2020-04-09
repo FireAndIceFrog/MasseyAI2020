8e9f91d836e776bf6c382bad65db3cf9f522f822
ChrisJMacdonald <chrisjmacdonald123@gmail.com>
2020-04-09 15:30:03 +1200

    Outline of AStar Completed

--- Assignment 1 Inator/program/AStar.cpp
+++ Assignment 1 Inator/program/AStar.cpp
@@ -21,14 +21,25 @@ class AStar {
 private:
 
     class node : public Puzzle {
+
     public:
         //Constructor: Takes a constant referance to a node and copies
         node(const node &p):Puzzle(p){}; 
         //Constructor: Constructs from Initial given state
         node(string const elements, string const goal):Puzzle(elements,goal){};
+
+        //Overriding the updateCost func
+        void updateCost(int hFunction){
+            //For A* Strict expanded List, Total cost is Path Length + Heuristic
+            //For our example, Each path moves the Blank tile by One Space
+            //Therefore, PathLength is equal to the Number of moves which is equal to depth
+            //Cost = depth + heuristic value
+            cost = h(hFunction) + depth;
+        }
+
         //Greater than Operator Overload for the Priority Queue
         bool operator> ( const node& rhs) const{
-	        return this->getHCost() > rhs.getHCost();
+	        return this->getCost() > rhs.getCost();
         }
     };
 
@@ -44,7 +55,43 @@ private:
 
 public:
 
-    
+    //Constructor for AStar Class
+    AStar(string start, string goal):maxQLen{0}, path{""},goal{goal}, init{start}, stateExpansions{0}{}
+    //Get AStar Path
+    string getPath(){
+        return path;
+    }
+    //Get Number of State expansions
+    int getStateExpansions(){
+        return stateExpansions;
+    }
+    //Get Maximum Queue Length
+    size_t getMaxQLen() {
+        return maxQLen;
+    }
+
+    void search(){
+        // (1) Initialise Q with search node (S) as only entry; set Expanded = ()
+
+        // (2) If Q is empty, fail.  Else, pick some search node N from Q.
+
+        // (3) Check if N is goal State. 
+        //              If it is, return N
+        //              If not, Take N from Q and Expand
+
+        // (4) If State N is already in Expanded List, Discard and move to step 2
+
+        // (5) Find all children of N (Not in expanded) and create them
+
+        // (6) Add all the extended paths, if Child already in Q, keep Smaller F Cost
+
+        // (7) Go to 2
+
+
+
+    }
+
+
 
 };
 #endif
